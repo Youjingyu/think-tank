08a9476e931feb34b9a48f7e8acd8be53b76be5f
whale <735284268@qq.com>
2017-08-26 22:08:17 +0800

    change p tag line-height in investors

--- js/setRootSize.js
+++ js/setRootSize.js
@@ -1,17 +1,18 @@
 (function (doc, win) {
-    var docEl = doc.documentElement,
-        //resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth || clientWidth < 1024) return;
-            docEl.style.fontSize = clientWidth * (100 / 1920) + 'px';
-        };
-    if (doc.addEventListener){
-        win.addEventListener('resize', recalc, false);
-        //doc.addEventListener('DOMContentLoaded', recalc, false);
-    } else{
-        win.attachEvent('onresize', recalc);
-        //doc.attachEvent('onDOMContentLoaded', recalc);
-    }
-    recalc();
+    doc.documentElement.style.fontSize = 100 + 'px';
+    //var docEl = doc.documentElement,
+    //    //resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    //    recalc = function () {
+    //        var clientWidth = docEl.clientWidth;
+    //        if (!clientWidth || clientWidth < 1024) return;
+    //        docEl.style.fontSize = clientWidth * (100 / 1920) + 'px';
+    //    };
+    //if (doc.addEventListener){
+    //    win.addEventListener('resize', recalc, false);
+    //    //doc.addEventListener('DOMContentLoaded', recalc, false);
+    //} else{
+    //    win.attachEvent('onresize', recalc);
+    //    //doc.attachEvent('onDOMContentLoaded', recalc);
+    //}
+    //recalc();
 })(document, window);
